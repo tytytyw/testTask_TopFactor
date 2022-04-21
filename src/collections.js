66ad1ccdb5616de46af4aad1aadec16f944880f4
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2022-04-21 16:45:39 +0300

    add collections

---
+++ src/collections.js
@@ -0,0 +1,15 @@
+export const data = [
+    { id: 0, date: { month: 'Апрель', year: 2021 } },
+    { id: 1, date: { month: 'Май', year: 2021 } },
+    { id: 2, date: { month: 'Июнь', year: 2021 } },
+    { id: 3, date: { month: 'Июль', year: 2021 } },
+    { id: 4, date: { month: 'Август', year: 2021 } },
+    { id: 5, date: { month: 'Сентябрь', year: 2021 } },
+    { id: 6, date: { month: 'Октябрь', year: 2021 } },
+    { id: 7, date: { month: 'Ноябрь', year: 2021 } },
+    { id: 8, date: { month: 'Декабрь', year: 2021 } },
+    { id: 9, date: { month: 'Январь', year: 2022 } },
+    { id: 10, date: { month: 'Февраль', year: 2022 } },
+    { id: 11, date: { month: 'Март', year: 2022 } },
+    { id: 12, date: { month: 'Апрель', year: 2022 } },
+]
